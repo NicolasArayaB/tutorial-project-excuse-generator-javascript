7860e3d7944b26458b06ec4fcebf1e16374e0700
NicolasArayaB <nicolasaraya@gmail.com>
2021-01-20 12:32:34 +0000

    hardcoded variables changed

--- main.js
+++ main.js
@@ -3,13 +3,14 @@ let action = ["ate ", "peed ", "crushed ", "broke "];
 let what = ["my homework ", "the keys ", "the car "];
 let when = ["before the class ", "right on time ", "when I finished ", "during my lunch ", "when I was playing "];
 
-function rand(max, min) {
+function rand(max, min = 0) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function excuseGen() {
   let excuse = document.getElementById("excuse");
-  excuse.innerHTML = who[rand(3, 0)] + action[rand(3, 0)] + what[rand(2, 0)] + when[rand(4, 0)];
+ 
+  excuse.innerHTML = who[rand(who.length)] + action[rand(action.length)] + what[rand(what.length)] + when[rand(when.length)];
 }
 
 window.onload = function() {
